be65f939effafa7c10c8e83dab8937ded46457b7
shibzz <monsterbegin@gmail.com>
2026-02-14 15:43:00 +0530

    feat(admin): Admin HOC to protect admin routes

--- frontend/src/components/AdminRoute.jsx
+++ frontend/src/components/AdminRoute.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useAuth } from "../context/AuthContext";
+import { Navigate, Outlet } from "react-router-dom";
 
 const AdminRoute = () => {
   const { isAdmin, user } = useAuth();
 
-  return user && isAdmin ? <outlet /> : <Navigate to="/login" replace />;
+  return user && isAdmin ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
 export default AdminRoute;
