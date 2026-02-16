d97de515c6a64131797b569c069026bd2e935e3e
shibzz <monsterbegin@gmail.com>
2026-02-16 11:29:50 +0530

    feat(orderRoutes): Add update order route for admin and refactor getAllOrders function

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,20 +1,5 @@
 import { Order } from "../models/Order.js";
 
-// Get allOrders
-export const getAllOrders = async (req, res) => {
-  try {
-    const allOrders = await Order.find({})
-      .populate("items.product")
-      .sort({ createdAt: -1 });
-
-    if (!allOrders || allOrders.length === 0) return res.status(200).json([]);
-
-    return res.status(200).json(allOrders);
-  } catch (error) {
-    return res.status(500).json({ message: "Failed to fetch all orders" });
-  }
-};
-
 // Get order
 export const getOrders = async (req, res) => {
   try {
@@ -68,5 +53,51 @@ export const createOrder = async (req, res) => {
     return res
       .status(500)
       .json({ message: "Order creation failed", error: error.message });
+  }
+};
+
+// Get allOrders **admin**
+export const getAllOrders = async (req, res) => {
+  try {
+    const allOrders = await Order.find({})
+      .populate("items.product")
+      .sort({ createdAt: -1 });
+
+    if (!allOrders || allOrders.length === 0) return res.status(200).json([]);
+
+    return res.status(200).json(allOrders);
+  } catch (error) {
+    return res.status(500).json({ message: "Failed to fetch all orders" });
+  }
+};
+
+// Update order **admin**
+export const updateOrder = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { status } = req.body;
+
+    const allowedStatuses = ["Processing", "Shipped", "Delivered", "Cancelled"];
+
+    if (!allowedStatuses.includes(status)) {
+      return res.status(400).json({ message: "Invalid status value" });
+    }
+
+    const order = await Order.findByIdAndUpdate(
+      id,
+      { status },
+      { new: true, runValidators: true },
+    );
+
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+
+    return res.status(200).json(order);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Failed to update order", error: error.message });
   }
 };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,6 +3,7 @@ import {
   createOrder,
   getAllOrders,
   getOrders,
+  updateOrder,
 } from "../controllers/orderController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 import { admin } from "../middlewares/adminMiddleware.js";
@@ -12,5 +13,8 @@ const router = express.Router();
 router.get("/all", protect, admin, getAllOrders);
 router.get("/", protect, getOrders);
 router.post("/", protect, createOrder);
+
+// admin routes
+router.patch("/:id", protect, admin, updateOrder);
 
 export default router;
